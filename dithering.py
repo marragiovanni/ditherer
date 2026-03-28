608ee861f405cdbd59695b7e3481410ebf53b4f3
Giovanni Marra <60482741+marragiovanni@users.noreply.github.com>
2026-03-28 18:40:41 +0100

    Refactor dithering functions and optimize imports

--- dithering.py
+++ dithering.py
@@ -1,66 +1,67 @@
-
-from PyQt6.QtGui import * 
-from PIL import Image
 import numpy as np
-from utils import *
+from PIL import Image
+from numba import njit
+from utils import matrices
 
 def load_img_as_rgb(imagePath): 
-    image = Image.open(imagePath).convert('RGB')
-    return image
+    return Image.open(imagePath).convert('RGB')
+
+@njit 
+def _apply_floyd_steinberg(img, h, w, intensity): 
+    for y in range(h): 
+        for x in range(w): 
+            oldpixel = img[y, x]
+            # Fast quantize 0 to 255
+            newpixel = 255.0 if oldpixel > 127.5 else 0.0 
+            img[y, x] = newpixel
+
+            error = (oldpixel - newpixel) * intensity 
+
+            if x + 1 < w: 
+                img[y, x+1] += error * 7 / 16
+            if y + 1 < h: 
+                if x > 0: 
+                    img[y+1, x-1] += error * 3 / 16
+                img[y+1, x] += error * 5 / 16
+                if x + 1 < w: 
+                    img[y+1, x+1] += error * 1 / 16
+
+    return img 
+
+def floyd_steinberg_dither(image, intensity):
+    img = np.array(image, dtype=np.float32) 
+    h, w = img.shape 
+
+    result = _apply_floyd_steinberg(img, h, w, intensity) 
+    return Image.fromarray(np.clip(result, 0, 255).astype(np.uint8))
+
 
 def random_dither(image, intensity): 
     img = np.array(image, dtype=np.uint8)
-    noise = np.random.randint(0, 256, size=img.shape, dtype=np.uint8) * intensity
+    
+    noise = np.random.uniform(0, 255, size=img.shape) * intensity
 
     dithered = np.where(img > noise, 255, 0).astype(np.uint8)
 
     return Image.fromarray(dithered)
 
  
-def bayer_dither(image, idx, threshold_value): 
+def bayer_dither(image, idx, threshold_value=128): 
     img = np.array(image, dtype = np.float32) / 255.0
-
     h, w = img.shape
-    matrix_size = matrices[idx].shape[0]
 
-    threshold_map = np.tile(matrices[idx], (h // matrix_size + 1, w // matrix_size + 1))
+    m = matrices[idx]
+    m_h, m_w = m.shape
+    
+    threshold_map = np.tile(m, (int(np.ceil(h/m_h)), int(np.ceil(w/m_w)))) 
     threshold_map = threshold_map[:h, :w]
-
-    dithered_array = (img>threshold_map).astype(np.uint8) * threshold_value
-        
-    dithered_image = Image.fromarray(dithered_array, mode='L')
     
-    return dithered_image
+    offset = (threshold_value / 255.0) - 0.5
+    threshold_map = np.clip(threshold_map - offset, 0.0, 1.0) 
 
-
-def floyd_steinberg_dither(image, mode, intensity): 
-    img = np.array(image, dtype=np.float32) 
-
-    if mode == "L": 
-        h, w, = img.shape
-    elif mode == "RGB":
-        h, w, _ = img.shape 
-
-    diffusion = [
-        (1, 0, 7 / 16),
-        (-1, 1, 3 / 16),
-        (0, 1, 5 / 16),
-        (1, 1, 1 / 16)
-    ]
-
-    for y in range(h): 
-        for x in range(w): 
-            oldpixel = img[y, x].copy()
-            newpixel = np.round(oldpixel / 255) * 255
-            img[y, x] = newpixel
-            error = (oldpixel - newpixel) * intensity
-            
-            for dx, dy, factor in diffusion: 
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < w and 0 <= ny < h: 
-                    img[ny, nx] += error * factor
-    return Image.fromarray(np.clip(img, 0, 255).astype(np.uint8))
-
-
-
+    dithered_array = (img>threshold_map).astype(np.uint8) * 255 
     
+    return Image.fromarray(dithered_array, mode='L') 
+
+
+
